5675a8311ef40114730ad38564de4b4117d3ca99
ELTEGANI <tiganimoh@gmail.com>
2023-11-13 18:51:36 +0400

    add view for displaying the list of games

--- app/src/main/java/com/example/games/presentation/navigation/Navigation.kt
+++ app/src/main/java/com/example/games/presentation/navigation/Navigation.kt
@@ -1,6 +1,13 @@
 package com.example.games.presentation.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -9,16 +16,36 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.games.presentation.components.GameScreen
 import com.example.games.presentation.navigation.screens.Screen
+import com.example.games.presentation.state.UiEffect
 import com.example.games.presentation.viewmodels.FreeGamesViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Navigation(navController: NavHostController){
-    NavHost(navController = navController, startDestination = Screen.GameScreen.route){
-        composable(Screen.GameScreen.route){
-            val freeGameViewModel = hiltViewModel<FreeGamesViewModel>()
-            val freeGamesState = freeGameViewModel.freeGamesState.collectAsStateWithLifecycle()
-            GameScreen(freeGameState = freeGamesState.value, modifier = Modifier)
+    val snackBarHostState = remember {
+        SnackbarHostState()
+    }
+    Scaffold(snackbarHost = {SnackbarHost(hostState = snackBarHostState)}) { paddingValues ->
+        NavHost(navController = navController, startDestination = Screen.GameScreen.route,
+            modifier = Modifier.padding(paddingValues)){
+            composable(Screen.GameScreen.route){
+                val freeGameViewModel = hiltViewModel<FreeGamesViewModel>()
+                val freeGamesState = freeGameViewModel.freeGamesState.collectAsStateWithLifecycle()
+
+                GameScreen(freeGameState = freeGamesState.value, modifier = Modifier)
+                LaunchedEffect(key1 =true){
+                    freeGameViewModel.uiEffect.collectLatest {
+                        when(it){
+                            UiEffect.NavigateToDetailScreen -> TODO()
+                            is UiEffect.ShowSnackBar -> {
+                                snackBarHostState.showSnackbar(it.msg)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
